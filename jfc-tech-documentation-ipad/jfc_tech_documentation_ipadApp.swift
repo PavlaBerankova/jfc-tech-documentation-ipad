1026431892f2d87338b64b241e0643406f64807e
Pavla Beránková <berankovap@protonmail.com>
2023-09-28 20:55:51 +0200

    Initial Commit

---
+++ jfc-tech-documentation-ipad/jfc_tech_documentation_ipadApp.swift
@@ -0,0 +1,17 @@
+//
+//  jfc_tech_documentation_ipadApp.swift
+//  jfc-tech-documentation-ipad
+//
+//  Created by Pavla Beránková on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct jfc_tech_documentation_ipadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
